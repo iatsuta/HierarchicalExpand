39316716b31e840625057a70e9b70f563e3007da
iatsuta <98311820+iatsuta@users.noreply.github.com>
2025-12-05 09:10:16 +0100

    add di (#1)

---
+++ src/HierarchicalExpand/DependencyInjection/ServiceCollectionExtensions.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace HierarchicalExpand.DependencyInjection;
+
+public static class ServiceCollectionExtensions
+{
+	extension(IServiceCollection services)
+	{
+		public IServiceCollection AddHierarchicalExpand(Action<IHierarchicalExpandSettings>? setupAction = null)
+		{
+			var settings = new HierarchicalExpandSettings();
+
+			setupAction?.Invoke(settings);
+
+			settings.Initialize(services);
+
+			return services;
+		}
+	}
+}
