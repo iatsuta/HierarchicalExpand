43e18fc68c5e7b813503ce2a5d2b70fa8da52db2
iatsuta <terraghost@mail.ru>
2025-12-05 15:13:09 +0100

    upd-libs

--- src/__SolutionItems/CommonAssemblyInfo.cs
+++ src/__SolutionItems/CommonAssemblyInfo.cs
@@ -3,7 +3,7 @@
 [assembly: AssemblyProduct("HierarchicalExpand")]
 [assembly: AssemblyCompany("IvAt")]
 
-[assembly: AssemblyVersion("1.6.1.1")]
+[assembly: AssemblyVersion("1.6.2.0")]
 [assembly: AssemblyInformationalVersion("changes at build")]
 
 #if DEBUG
