ea09365bd545b67cca37cc208a64c48051f1c016
iatsuta <98311820+iatsuta@users.noreply.github.com>
2026-02-04 11:26:02 +0100

    upd AncestorLinkInfo

--- src/HierarchicalExpand.Abstractions/AncestorLinkInfo.cs
+++ src/HierarchicalExpand.Abstractions/AncestorLinkInfo.cs
@@ -5,15 +5,15 @@ using CommonFramework;
 namespace HierarchicalExpand;
 
 public record AncestorLinkInfo<TDomainObject, TAncestorLink>(
-	PropertyAccessors<TAncestorLink, TDomainObject> From,
-	PropertyAccessors<TAncestorLink, TDomainObject> To)
+    PropertyAccessors<TAncestorLink, TDomainObject> From,
+    PropertyAccessors<TAncestorLink, TDomainObject> To)
 {
-	public AncestorLinkInfo(
-		Expression<Func<TAncestorLink, TDomainObject>> fromPath,
-		Expression<Func<TAncestorLink, TDomainObject>> toPath)
-		: this(new PropertyAccessors<TAncestorLink, TDomainObject>(fromPath), new PropertyAccessors<TAncestorLink, TDomainObject>(toPath))
-	{
-	}
+    public AncestorLinkInfo(
+        Expression<Func<TAncestorLink, TDomainObject>> fromPath,
+        Expression<Func<TAncestorLink, TDomainObject>> toPath)
+        : this(fromPath.ToPropertyAccessors(), toPath.ToPropertyAccessors())
+    {
+    }
 
-	public AncestorLinkInfo<TDomainObject, TAncestorLink> Reverse() => new(this.To.Path, this.From.Path);
+    public AncestorLinkInfo<TDomainObject, TAncestorLink> Reverse() => new(this.To, this.From);
 }
