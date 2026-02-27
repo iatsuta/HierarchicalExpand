c88dbebfddaeef274cdbfcbb7246796a969a0781
iatsuta <98311820+iatsuta@users.noreply.github.com>
2026-02-27 18:35:42 +0100

    upd DomainObjectExpander (#23)

--- src/HierarchicalExpand.Runtime/DomainObjectExpander.cs
+++ src/HierarchicalExpand.Runtime/DomainObjectExpander.cs
@@ -12,15 +12,39 @@ public class DomainObjectExpander<TDomainObject>(HierarchicalInfo<TDomainObject>
 
     public async Task<HashSet<TDomainObject>> GetAllParents(IEnumerable<TDomainObject> startDomainObjects, CancellationToken cancellationToken)
     {
-        var cache = await this.GetCache(cancellationToken);
-
         var allResult = startDomainObjects.ToHashSet();
+
+        if (allResult.Count == 1)
+        {
+            return await this.GetAllParents(allResult.Single(), cancellationToken);
+        }
+        else if (allResult.Count != 0)
+        {
+            var cache = await this.GetCache(cancellationToken);
+
+            for (var nextLayer = allResult; nextLayer.Any(); allResult.UnionWith(nextLayer))
+            {
+                nextLayer = cache.Where(pair => nextLayer.Contains(pair.Key) && pair.Value != null)
+                    .Select(pair => pair.Value!)
+                    .ToHashSet();
+            }
+        }
+
+        return allResult;
+    }
+
+    private async Task<HashSet<TDomainObject>> GetAllParents(TDomainObject startDomainObject, CancellationToken cancellationToken)
+    {
+        var allResult = new[] { startDomainObject }.ToHashSet();
 
         for (var nextLayer = allResult; nextLayer.Any(); allResult.UnionWith(nextLayer))
         {
-            nextLayer = cache.Where(pair => nextLayer.Contains(pair.Key) && pair.Value != null)
-                .Select(pair => pair.Value!)
-                .ToHashSet();
+            nextLayer = await queryableSource.GetQueryable<TDomainObject>()
+                .Where(currentObj => nextLayer.Contains(currentObj))
+                .Select(hierarchicalInfo.ParentPath)
+                .Where(nextObj => nextObj != null && !allResult.Contains(nextObj))
+                .Select(nextObj => nextObj!)
+                .GenericToHashSetAsync(cancellationToken);
         }
 
         return allResult;
@@ -28,24 +52,27 @@ public class DomainObjectExpander<TDomainObject>(HierarchicalInfo<TDomainObject>
 
     public async Task<HashSet<TDomainObject>> GetAllChildren(IEnumerable<TDomainObject> startDomainObjects, CancellationToken cancellationToken)
     {
-        var cache = await this.GetCache(cancellationToken);
-
         var allResult = startDomainObjects.ToHashSet();
 
-        for (var nextLayer = allResult; nextLayer.Any(); allResult.UnionWith(nextLayer))
+        if (allResult.Count != 0)
         {
-            nextLayer = cache.Where(pair => pair.Value != null && nextLayer.Contains(pair.Value))
-                .Select(pair => pair.Key)
-                .ToHashSet();
+            var cache = await this.GetCache(cancellationToken);
+
+            for (var nextLayer = allResult; nextLayer.Any(); allResult.UnionWith(nextLayer))
+            {
+                nextLayer = cache.Where(pair => pair.Value != null && nextLayer.Contains(pair.Value))
+                    .Select(pair => pair.Key)
+                    .ToHashSet();
+            }
         }
 
         return allResult;
     }
 
     private async Task<IReadOnlyDictionary<TDomainObject, TDomainObject?>> GetCache(CancellationToken cancellationToken) =>
-        cached ? this.baseCache ??= await this.CalcCacheValue(cancellationToken) : await this.CalcCacheValue(cancellationToken);
+        cached ? this.baseCache ??= await this.CreateCache(cancellationToken) : await this.CreateCache(cancellationToken);
 
-    private async Task<Dictionary<TDomainObject, TDomainObject?>> CalcCacheValue(CancellationToken cancellationToken) =>
+    private async Task<Dictionary<TDomainObject, TDomainObject?>> CreateCache(CancellationToken cancellationToken) =>
         await queryableSource.GetQueryable<TDomainObject>().WithFetch(r => r.Fetch(hierarchicalInfo.ParentPath))
             .GenericToDictionaryAsync(d => d, hierarchicalInfo.ParentFunc, cancellationToken);
 }
